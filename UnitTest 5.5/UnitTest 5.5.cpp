62ff25cb70363439ca9ec78ebf75335854fdb1fc
Forever-Incognito <andrii2003mak@gmail.com>
2020-11-21 18:21:04 +0200

    First Test

---
+++ UnitTest 5.5/UnitTest 5.5.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Лабораторна 5.5/Source.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest55
+{
+	TEST_CLASS(UnitTest55)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = gcd(6, 2);
+			Assert::AreEqual(t, 2);
+		}
+	};
+}
