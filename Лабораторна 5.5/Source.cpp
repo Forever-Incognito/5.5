f106d617c0df87825dc551b739ec77cff569f9c3
Forever-Incognito <andrii2003mak@gmail.com>
2020-11-21 17:37:04 +0200

    a

---
+++ Лабораторна 5.5/Source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int gcd(int n, int m)
+{
+    if (n == 0)
+        return m;
+    if (m % n == 0)
+        return n;
+    if (n == m)
+        return n;
+    if (n > m)
+        return gcd(n - m, m);
+    return gcd(n, m - n);
+}
+int gcd2(int r, int m)
+{
+    if (r == 0)
+        return m;
+    if (r % m == 0)
+        return r;
+    if (r == m)
+        return r;
+    if (r > m)
+        return gcd(r - m, m);
+    return gcd(r, m - r);
+}
+
+int main()
+{
+    int n, m;
+    cout << "n = "; cin >> n;
+    cout << "m = "; cin >> m;
+    int r;
+    r = n % m;
+    cout << "gcd(n,m)" << " is " << gcd(n, m);
+    cout << endl;
+    cout << "gcd(m,r)" << " is " << gcd2(r, m);
+    cout << endl;
+    cout << "GCD of " << n << " and " << m << " is " << gcd(n, m);
+    cout << endl;
+    cout << "GCD2 of " << m << " and " << r << " is " << gcd2(r, m);
+    return 0;
+}
